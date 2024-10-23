7014a5eb887fb0d257c22975109fe902ce9891c9
kishori-pathrabe <kishori-yogeshwar.pathrabe@capgemini.com>
2024-10-23 17:07:20 +0530

    Update heading.js

--- blocks/heading/heading.js
+++ blocks/heading/heading.js
@@ -1,6 +1,6 @@
 // Get all divs within the blue class
 const nestedDivs = document.getElementsByClassName('blue')[0].getElementsByTagName('div');
-
+console.log(nestedDivs);
 // Check if there's at least one nested div
 if (nestedDivs.length > 0) {
     const firstParagraph = nestedDivs[0].getElementsByTagName('p')[0];
