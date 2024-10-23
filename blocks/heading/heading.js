5963bbdadce963f6ca86f8cb021f4a7cc1eeed55
kishori-pathrabe <kishori-yogeshwar.pathrabe@capgemini.com>
2024-10-23 18:45:25 +0530

    Update heading.js

--- blocks/heading/heading.js
+++ blocks/heading/heading.js
@@ -1,12 +1,12 @@
-// Get all divs within the blue class
-const nestedDivs = document.getElementsByClassName('blue')[0].getElementsByTagName('div');
-console.log(nestedDivs);
-// Check if there's at least one nested div
-if (nestedDivs.length > 0) {
-    const firstParagraph = nestedDivs[0].getElementsByTagName('p')[0];
+// // Get all divs within the blue class
+// const nestedDivs = document.getElementsByClassName('blue')[0].getElementsByTagName('div');
+// console.log(nestedDivs);
+// // Check if there's at least one nested div
+// if (nestedDivs.length > 0) {
+//     const firstParagraph = nestedDivs[0].getElementsByTagName('p')[0];
 
-    // Change the style of the first nested paragraph
-    if (firstParagraph) {
-        firstParagraph.style.backgroundColor = '#0070c0'; // Change background color
-    }
-}
+//     // Change the style of the first nested paragraph
+//     if (firstParagraph) {
+//         firstParagraph.style.backgroundColor = '#0070c0'; // Change background color
+//     }
+// }
