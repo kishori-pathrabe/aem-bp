9324ae4e5da73ef89a62d3fc13e6ea9cbd74bf36
kishori-pathrabe <kishori-yogeshwar.pathrabe@capgemini.com>
2024-10-24 17:02:31 +0530

    Update links.js

--- blocks/links/links.js
+++ blocks/links/links.js
@@ -1,37 +1,12 @@
-export function decorateMain(main) {
-    // hopefully forward compatible button decoration
-    decorateButtons(main);
-    decorateIcons(main);
-    buildAutoBlocks(main);
-    decorateSections(main);
-    decorateBlocks(main);
-    decorateBookmarkLinks(main)
-}
- 
-/**
-* Decorates decorate BookmarkLinks links to absolute.
-* @param {Element} main The container element
-*/
-export function decorateBookmarkLinks(main) {
-    main.querySelectorAll('a').forEach((a) => {
-        const href = a.getAttribute('href'); if (href) { // Check if the href contains 'bookmark' if (href.includes('bookmark')) { // Convert to a relative URL by removing the domain const relativeUrl = href.replace(/^.*\/\/[^\/]+/, ''); a.setAttribute('href', relativeUrl); console.log(`Converted to relative URL: ${relativeUrl}`); } } });
-}
-export function decorateLinkedPictures(container, processInBlocks = true) {
-  /* MS Word online does not support linked images. As a workaround use any links
-  that are directly after the image. */
+const paragraph = document.querySelector('.demo');
+const content = paragraph.textContent;
 
-  // picture + br + a in the same paragraph
-  [...container.querySelectorAll('picture + br + a, picture + a')]
-  // link text is an unformatted URL paste
-    .filter((a) => a.textContent.trim().startsWith('http'))
-    .forEach((a) => {
-      const br = a.previousElementSibling;
-      let picture = br.previousElementSibling;
-      if (br.tagName === 'PICTURE') picture = br;
-      picture.remove();
-      br.remove();
-      a.innerHTML = picture.outerHTML;
-      // make sure the link is not decorated as a button
-      a.parentNode.classList.remove('button-container');
-      a.className = '';
-    });
+// Use a regular expression to find the URL starting with "https://"
+const urlMatch = content.match(/https:\/\/[^\s]+/);
+
+if (urlMatch) {
+    const url = urlMatch[0]; // The first matched URL
+    const link = `<a href="${url}" target="_blank">${url}</a>`; // Create the anchor tag
+    const updatedContent = content.replace(url, link); // Replace the URL with the anchor tag
+    paragraph.innerHTML = updatedContent;
+}
