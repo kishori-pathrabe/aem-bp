b30d85ea3f8686d9238de2f9197fc9ba615a7f0c
kishori-pathrabe <kishori-yogeshwar.pathrabe@capgemini.com>
2024-10-28 16:57:31 +0530

    Update heading.js

--- blocks/heading/heading.js
+++ blocks/heading/heading.js
@@ -1,16 +1,24 @@
-const ul = document.querySelector('.default-content-wrapper ul');
+document.addEventListener('DOMContentLoaded', function() {
+    const ul = document.querySelector('.default-content-wrapper ul');
 
-// Select the paragraph that you want to convert into a list item
-const paragraph = document.querySelector('.default-content-wrapper p:nth-of-type(2)');
+    // Get all siblings of the <ul> that are <p> elements
+    const siblings = Array.from(ul.parentElement.children);
+    
+    // Loop through each sibling after the <ul>
+    let foundUl = false;
+    siblings.forEach(sibling => {
+        if (sibling === ul) {
+            foundUl = true; // Mark that we found the <ul>
+        } else if (foundUl && sibling.tagName === 'P') {
+            // Create a new list item (li)
+            const li = document.createElement('li');
+            // Move the content of the paragraph into the new list item
+            li.innerHTML = sibling.innerHTML;
 
-// Create a new list item (li)
-const li = document.createElement('li');
-
-// Move the content of the paragraph into the new list item
-li.innerHTML = paragraph.innerHTML;
-
-// Append the new list item to the unordered list
-ul.appendChild(li);
-
-// Optionally, remove the original paragraph
-paragraph.remove();
+            // Append the new list item to the unordered list
+            ul.appendChild(li);
+            // Optionally, remove the original paragraph
+            sibling.remove();
+        }
+    });
+});
