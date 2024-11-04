b9aba5e7075d25ef17145324e44b019e250fac29
kishori-pathrabe <kishori-yogeshwar.pathrabe@capgemini.com>
2024-11-04 14:08:47 +0530

    Update hotspot.js
    break code

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -1,1 +1,34 @@
+export default function decorate(block) {
+[...block.children].forEach((row, r) => {
+    if (r > 0) {
+      const content = [...row.children][0].textContent.trim();
+      const variant = block.classList.value;  
+      const nexticondiv = document.createElement('div');
+      nexticondiv.classList.add('hotspot'); // Added class for CSS targeting
+      nexticondiv.style.left = [...row.children][1].textContent;
+      nexticondiv.style.top = [...row.children][2].textContent;
+      nexticondiv.setAttribute('data', content);
+      const contentContainer = document.createElement('div');
+      contentContainer.classList.add('hotspot-content');
+      if (isTextVariant) {
+        contentContainer.textContent = content; // Display text
+        contentContainer.classList.add('bgborder');
+      }
+       // Append content container to hotspot div
+      nexticondiv.appendChild(contentContainer);
+      nexticondiv.addEventListener('click', () => {
+      // Hide content of all other hotspots
+        document.querySelectorAll('.hotspot').forEach((hotspot) => {
+          if (hotspot !== nexticondiv) {
+            hotspot.classList.remove('onclick');
+          }
+           // Toggle the current hotspot content
+        nexticondiv.classList.toggle('onclick');
+      });
 
+      row.after(nexticondiv);
+      row.remove();
+    }
+  });
+
+}
